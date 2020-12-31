52f418c10761f4a3347668ec9496afa9e9d4fe8d
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2020-12-31 01:32:02 -0600

    added bootstrap and tabs on menu page - NW

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import {Tab, Tabs} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // Menu component
 
 class Menu extends Component {
     render(){
       return(
         <div>
+          <Tabs defaultActiveKey='profile' id='uncontrolled-tab-example'>
+            <Tab eventKey="appetizers" title="Appetizers">
+              <p>Lorem Ipsum Menu Apps</p>
+            </Tab>
+            <Tab eventKey="entrees" title="Entrees">
+              <p>Lorem Ipsum Menu Entrees</p>
+            </Tab>
+            <Tab eventKey="desserts" title="Desserts">
+              <p>Lorem Ipsum Yummy Desserts</p>
+            </Tab>
+          </Tabs>
           <h1>Menu</h1>
           <p>Check out our amazing 24/7 menu:
             <ul>
@@ -13,6 +26,14 @@ class Menu extends Component {
               <li>Location Transition Handling</li>
             </ul>
           </p>
+          <h1>Appetizers</h1>
+            <ul>
+              <li>Baked Brussel Sprouts</li>
+              <li>Baked Asparagus</li>
+              <li>Edamame</li>
+            </ul>
+          <h1>Entrees</h1>
+          <h1>Desserts</h1>
         </div>
       );
     }
